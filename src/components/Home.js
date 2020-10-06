b968ffd3d408cb12a07e809a8ad66cb3a193f09d
Jordan <jordanleelong5@gmail.com>
2020-10-06 15:26:40 -0700

    added name, position, icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react' 
-
+import { Linkedin } from 'react-feather'
+import { GitHub } from 'react-feather'
 class Home extends React.Component {
     render() {
          return (
              <div>
                  Home page
+                 <h1>Front end developer </h1>
+                 <h3>Jordan Long</h3>
+                 {/* icons for linkedin, github */}    
+            <Linkedin />              
+            <GitHub />
              </div>
          )
     };
