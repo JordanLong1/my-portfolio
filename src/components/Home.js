964fa13f22ca848809cafe93247f9ff1a77783ec
Jordan <jordanleelong5@gmail.com>
2020-10-06 17:10:17 -0700

    imported Navbar changed font color

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,28 @@
 import React from 'react' 
 import { Linkedin } from 'react-feather'
 import { GitHub } from 'react-feather'
+import styles from '../Home.css'
+import Navbar from './Navbar'
 class Home extends React.Component {
+
     render() {
          return (
-             <div>
-                 Home page
-                 <h1>Front end developer </h1>
-                 <h3>Jordan Long</h3>
-                 {/* icons for linkedin, github */}    
-            <Linkedin />              
-            <GitHub />
-             </div>
+            <div className='html'>
+                <div>
+                    <Navbar />
+                </div>
+            <div className='stars'>
+                <div id='stars2'></div>
+                <div id='stars3'></div>
+                <div id='title'></div>
+                 <h1 style={{color: 'white'}}>Jordan Long</h1>   
+                    <h3 style={{color: 'white'}}>Front end developer </h3>
+             <Linkedin onClick={this.handleLinkedInClick} style={{color: 'white'}} />              
+             <GitHub style={{color: 'white'}}/>
+            </div>
+
+        </div>
+           
          )
     };
 };
