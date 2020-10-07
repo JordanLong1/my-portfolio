a7e9b29cdfbd576febe97887d7a9117a2fd8e85c
Jordan <jordanleelong5@gmail.com>
2020-10-07 10:42:04 -0700

    got icons to show on the page

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,10 +1,13 @@
 import React from 'react' 
-
+import {Linkedin, GitHub, Mail } from 'react-feather'
 class ContactMe extends React.Component {
     render() {
         return (
             <div>
-                Contact Me
+                <h1 className='contact header'> Contact Me </h1>
+            <Linkedin />
+            <GitHub />
+            <Mail />
             </div>
         )
     }
