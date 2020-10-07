29a9a487835c54655614015ed09647e79768d8bc
Jordan <jordanleelong5@gmail.com>
2020-10-07 11:09:08 -0700

    got avatar to display

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import stylez from '../AboutMe.css'
+import Navbar from './Navbar'
 class AboutMe extends React.Component {
     render() {
         return (
             <div className='about-container'>
+                <Navbar />
                 <h1 className='about-me-header'>About Me </h1>
             
                 <div className='bio'>
@@ -25,7 +27,7 @@ class AboutMe extends React.Component {
                 </div>
 
                 <div className='image div'>
-                    {/* <img src='' alt=''>Image of myself here</img> */}
+                    <img className='avatar-pic' src='../profilepic.png' alt='' />
 
                 </div>
             </div>
