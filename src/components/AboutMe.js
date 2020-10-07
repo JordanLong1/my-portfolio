144c9edfa329439fa7f92ffecdb8f7c4d1f7cc8a
Jordan <jordanleelong5@gmail.com>
2020-10-07 10:41:40 -0700

    made divs for what ill need, imported stylesheet

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,10 +1,33 @@
 import React from 'react'
-
+import stylez from '../AboutMe.css'
 class AboutMe extends React.Component {
     render() {
         return (
-            <div>
-                About me!
+            <div className='about-container'>
+                <h1 className='about-me-header'>About Me </h1>
+            
+                <div className='bio'>
+                    <p> 
+                    This will be the bio
+                    </p>
+                </div>
+
+                <div className='frontend-skills'>
+                    frontend skills maybe throw icons
+                </div>
+
+                <div className='backend-skills'>
+                backend skills maybe throw some icons
+                </div>
+
+                <div className='interests'>
+            <p> list some interests</p>
+                </div>
+
+                <div className='image div'>
+                    {/* <img src='' alt=''>Image of myself here</img> */}
+
+                </div>
             </div>
         )
     }; 
