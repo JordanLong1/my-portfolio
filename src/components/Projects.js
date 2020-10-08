8ecc4db339b455f8b3f26c11c934dd84ef957ecc
Jordan <jordanleelong5@gmail.com>
2020-10-08 15:00:38 -0700

    skeleton for projects page with no images or styling set up

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,30 @@
 import React from 'react'
-
+import {GitHub, Youtube} from 'react-feather'
 class Projects extends React.Component {
     render() {
         return (
-            <div>
-                Projects page
+            <div className='projects-container'>
+                <h1 className='projects-header'>My Projects</h1>
+                <div className='project-one'>
+                    <p>Description of project one</p>
+                    {/* <img></img> */}
+                    <GitHub />
+                    <Youtube style={{color: 'red'}}/>
+                </div>
+                <div className='project-two'>
+                    {/* <img></img> */}
+                    <p>Description of project two</p>
+                    <GitHub />
+                    <Youtube style={{color: 'red'}}/>
+                </div>
+
+                <div className='project-three'>
+                    {/* <img></img> */}
+                    <p>Description of project 3</p>
+                    <GitHub /> 
+                    <Youtube style={{color: 'red'}}/>
+
+                </div>
             </div>
         )
     }
