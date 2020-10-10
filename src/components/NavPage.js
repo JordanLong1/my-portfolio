213d1332365e2250699280003e14a255b727a12c
Jordan <jordanleelong5@gmail.com>
2020-10-09 22:30:28 -0700

    added navpage

---
+++ src/components/NavPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class NavPage extends React.Component {
+    render() {
+        return (
+            <div>
+                NavPage
+            </div>
+        )
+    };
+};
+
+export default NavPage
