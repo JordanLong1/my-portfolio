ca6a3f57ba70591e8198fd27a4f583c5b4dbcf19
Jordan <jordanleelong5@gmail.com>
2020-10-09 22:34:45 -0700

    addded links for navpage

--- src/components/NavPage.js
+++ src/components/NavPage.js
@@ -1,10 +1,14 @@
 import React from 'react'
-
+import {Link } from 'react-router-dom'
 class NavPage extends React.Component {
     render() {
         return (
             <div>
-                NavPage
+            
+                <Link to='/home' >Home</Link><br></br>
+                <Link to='/projects'>Projects</Link><br></br>
+                <Link to='/contactme'>Contact Me</Link><br></br>
+                <Link to='/aboutme'>About Me</Link>
             </div>
         )
     };
