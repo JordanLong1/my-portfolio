a123aea431590867fbd5e4d9e9909724f6a574cc
Jordan <jordanleelong5@gmail.com>
2020-10-10 14:51:25 -0700

    wrapped links in div

--- src/components/NavPage.js
+++ src/components/NavPage.js
@@ -5,10 +5,13 @@ class NavPage extends React.Component {
     render() {
         return (
             <div className='navpage-container'>
+                <div className='nav-links'>
                 <Link to='/' >Home</Link><br></br>
                 <Link to='/projects'>Projects</Link><br></br>
                 <Link to='/contactme'>Contact Me</Link><br></br>
                 <Link to='/aboutme'>About Me</Link>
+
+                </div>
             </div>
         )
     };
