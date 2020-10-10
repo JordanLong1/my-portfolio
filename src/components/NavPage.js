2d28325d4515afd77ea4dfb4e5750cd5df65ae93
Jordan <jordanleelong5@gmail.com>
2020-10-10 14:47:34 -0700

    added classname to style background, imported css file

--- src/components/NavPage.js
+++ src/components/NavPage.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Link } from 'react-router-dom'
+import stylez from '../Home.css'
 class NavPage extends React.Component {
     render() {
         return (
-            <div>
-            
+            <div className='navpage-container'>
                 <Link to='/' >Home</Link><br></br>
                 <Link to='/projects'>Projects</Link><br></br>
                 <Link to='/contactme'>Contact Me</Link><br></br>
