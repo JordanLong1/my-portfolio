682b30d8764f02d4b67b299a976d5c391b2510e6
Jordan <jordanleelong5@gmail.com>
2020-10-12 18:18:32 -0700

    added classNames for custom styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,7 +7,7 @@ class Projects extends React.Component {
                 <h1 className='projects-header'>My Projects</h1>
                 <div className='project-one'>
                     <h4>Corner Store</h4>
-                    <p>Corner Store clothing was a shopping cart / basic e-commerce web app designed for users to add and subtract products from their shopping cart.</p>
+                    <p className='project-one-p-tag'>Corner Store clothing was a shopping cart / basic e-commerce web app designed for users to add and subtract products from their shopping cart.</p>
                     <label>Developed In:</label>
                     <ul>
                         <li>OO Javascript</li>
@@ -22,8 +22,8 @@ class Projects extends React.Component {
                 <div className='project-two'>
                     <h4>Innovative Ag Solutions</h4>
                     {/* <img></img> */}
-                    <p>Innovative Ag Solutions is a consultation web app for PCA'S (Pest Control Advisors) or salesman/representatives and farmers or growers in the agricultural industry.</p>
-                    <p>A grower can log-in add their particular crops and a salesman can login to their account and view and manage their growers crops. </p>
+                    <p className='project-two-p-tag'>Innovative Ag Solutions is a consultation web app for PCA'S (Pest Control Advisors) or salesman/representatives and farmers or growers in the agricultural industry.</p>
+                    <p className='project-two-p-tag'>A grower can log-in add their particular crops and a salesman can login to their account and view and manage their growers crops. </p>
                     <label>Developed In:</label>
                     <ul>
                         <li>Ruby on Rails API</li>
@@ -39,8 +39,8 @@ class Projects extends React.Component {
                 <div className='project-three'>
                     <h4>Optimal CRM</h4>
                     {/* <img></img> */}
-                    <p>Optimal CRM (Customer Relationship Management) system for managing companys relationships and interactions. </p>
-                    <p>A salesrep can login, schedule tasks with potential leads or upcoming meetings, and view the organizations they have already landed.</p>
+                    <p className='project-three-p-tag'>Optimal CRM (Customer Relationship Management) system for managing companys relationships and interactions. </p>
+                    <p className='project-three-p-tag'>A salesrep can login, schedule tasks with potential leads or upcoming meetings, and view the organizations they have already landed.</p>
                     <label>Developed In:</label>
                     <ul>
                         <li>Ruby on Rails</li>
