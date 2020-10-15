4c0566466ba8668d3ba4d4ba0f0a992ab9dc4c0d
Jordan <jordanleelong5@gmail.com>
2020-10-15 11:01:02 -0700

    instead of separating each project they are working with container & presentational now

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import ProjectOne from '../Projects/ProjectOne'
-import ProjectTwo from '../Projects/ProjectTwo'
-import ProjectThree from '../Projects/ProjectThree'
+import {Project} from '../Projects/Project'
 
 class Projects extends React.Component {
+    state = {frontCard: true }
+
     render() {
         return (
             <div className='projects-container'>
                 <h1 className='projects-header'>My Projects</h1>
-            <ProjectOne />
-            <ProjectTwo />
-            <ProjectThree />
+            <Project divClassName='project-one' title='Corner Store' titleDescription='Basic Shopping cart / e-commerce type with the ability to add and remove items to a from a cart.' description='+ Created the rails API to accomplish fetching product data and present it to the client. + Used OO Javascript to build out the frontend and fetch data from the API. + Implemented various CSS animations to refine the overall presentation of the project' image={''} technologies='OO Javascript, Ruby on Rails, HTML, CSS' />
+            <Project divClassName='project-two' title='Innovative Ag Solutions' titleDescription='Consultation web app for a PCA (Pest Control Advisor) to manage and consult their farmers/growers crops.' description='+ Centralized state management by using the Redux store to store all state information and for better scalability. + Engineered client side routing with BrowserRouter from the React-Router library to accelerate navigation. + Executed Single-Table-Inheritance to achieve the capability of having multiple user types (grower and salesman)' technologies='Ruby on Rails, React and Redux, Semantic UI for styling.'/>
+            <Project divClassName='project-three' title='Optimal CRM' titleDescription='Fake CRM with the ability for a salesrepresentative to keep track of leads, tasks, and companies.' description='+ Achieved the usage of model, view, controller (MVC) design pattern for separation of concerns within the application. + Demonstrated CRUD (create, read, update, delete) to implement a persistent storage application. + Maximized user security measures with implementing Bcrypt to safely store and salt user passwords.' technologies='Ruby on Rails'/>
             </div>
         )
     }
