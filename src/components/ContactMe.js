a76ff1fb66b8728b7d64dc2e82db1cc5866587f7
Jordan <jordanleelong5@gmail.com>
2020-10-19 13:56:45 -0700

    removed stars info, imported menu, onClick for menu

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,15 +1,24 @@
 import React from 'react' 
-import {Linkedin, GitHub, Mail} from 'react-feather'
+import {Linkedin, GitHub, Mail, Menu} from 'react-feather'
 import '../Home.css'
 class ContactMe extends React.Component {
 
+    componentDidMount() {
+        window.scrollTo(0, 0)
+    }
+
     handleClick = () => {
         window.location.href = "mailto:jordanleelong5@gmail.com"
+    }
+
+    handleMenu = () => {
+        this.props.history.push('/navpage')
     }
     
     render() {
         return (
             <div className='contact-container'>
+                <Menu className='hamburger' onClick={this.handleMenu}/>
                 <h1 className='header'> Contact Me </h1>
                 <a href='https://www.linkedin.com/in/jordan-long21/'> 
                 <Linkedin className='linked-icon' />
