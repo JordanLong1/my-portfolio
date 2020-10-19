17947867dab87b9a0ac48bd9dc4e1d13f522f1d7
Jordan <jordanleelong5@gmail.com>
2020-10-18 22:34:55 -0700

    changed welcome message

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,8 +17,7 @@ class Home extends React.Component {
                 <div id='stars2'></div>
                 <div id='stars3'></div>
                 <div id='title'></div>
-                 <h1 style={{color: 'white'}}>Jordan Long</h1>   
-                    <h3 style={{color: 'white'}}>Front end developer </h3>
+                 <h1 style={{color: 'white'}} className='opening-text'>Hello, welcome to my site. I'm Jordan Long and I am a frontend developer.</h1>   
              <a href='https://www.linkedin.com/in/jordan-long21/'> 
              <Linkedin style={{color: 'white'}} /> 
              </a>
