35928b32c28c13766bb83e220f01cf6046141971
Jordan <jordanleelong5@gmail.com>
2020-10-21 14:29:51 -0700

    separated things out by divs. added classNames for styling

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,37 +7,39 @@ class AboutMe extends React.Component {
         return (
             <div className='about-container'>
               
-                <h1 className='about-me-header'>About Me </h1>
+                <h1 className='about-me-header'>Hi, I'm Jordan. Nice to meet you.</h1>
             
                 <div className='bio'>
                     <Bio />
                 </div>
 
-                <div className='frontend-skills'>
-                   <h4>FrontEnd</h4>
+                <div className='skills-container'>
+
+                <div className='column-wrapper'>
+
+                <div className='skills-column'>
+                   <h4 className='skills-header'>FrontEnd</h4>
                    <li>Javascript</li>
                    <li>React</li>
                    <li>Redux</li>
                    <li>HTML</li>
                    <li>CSS</li>
                    <li>Bootstrap, Semantic UI</li>
-                </div>
+                    </div>
 
-                <div className='backend-skills'>
-                <h4>Backend</h4>
+                    <div className='skills-column'>
+                <h4 className='skills-header'>Backend</h4>
                 <li>Ruby on Rails</li>
                 <li>Postgres</li>
                 <li>SQLite</li>
-                </div>
-
-                <div className='interests'>
+                    </div>
+         
+            <div className='skills-column'>
+                <h4 className='skills-header'>Interests</h4>
             <p> list some interests</p>
-                </div>
-
-                {/* <div className='image div'>
-                    <img className='avatar-pic' src='../profilepic2.jpeg' alt='' />
-
-                </div> */}
+            </div>
+            </div>
+            </div>
             </div>
         )
     }; 
