2d862e7aff257f7ca7f85b8ac770f8c9c9f16ba4
Jordan <jordanleelong5@gmail.com>
2020-10-21 14:29:18 -0700

    added div for desk pic

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react' 
 import Navbar from './Navbar'
+import AboutMe from './AboutMe'
 import '../Home.css'
 class Home extends React.Component {
 
@@ -16,11 +17,17 @@ class Home extends React.Component {
                 <Navbar />
                  <h1  className='home-text'>Front-end Developer </h1>  
                  <h4 className='home-text'>I write code that users interact with, and I love what I do. </h4>
+
+                 <div>
                 <img className='avt' src={'../Avatar.png'} alt='' />
-                {/* <div>
+                 </div>
+                 <div className='desk-pic-div'>
+                <img  className='desk-pic' src={'../desk.png'} alt=''/>
+                 </div>
+                <div>
                 <AboutMe />
                 </div>
-                <div>
+                {/* <div>
                     <Projects />
                 </div> */}
 
