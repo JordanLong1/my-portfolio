a6e175a50d58f1a0eac4a1a8781c2500ca6b3f3c
Jordan <jordanleelong5@gmail.com>
2020-10-22 15:35:02 -0700

    added headers here instead of on the picure

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,6 +13,7 @@ class Projects extends React.Component {
                 <div className='projects-container'>
 
                     <div className='project-pics-and-info'>
+                        <h4 className='proj-title'>Corner Store Clothing</h4>
                     <ProjectPicture title='Corner Store' image={'../corner.png'} />
                     {/* <ProjectInfo titleDescription='Basic Shopping cart / e-commerce type with the ability to add and remove items to a from a cart.' description='+ Created the rails API to accomplish fetching product data and present it to the client. + Used OO Javascript to build out the frontend and fetch data from the API. + Implemented various CSS animations to refine the overall presentation of the project' technologies='OO Javascript, Ruby on Rails, HTML, CSS' /> */}
                     <button className='git-btn'>Github</button>
@@ -20,6 +21,7 @@ class Projects extends React.Component {
                     </div>
 
                     <div className='project-pics-and-info'>
+                    <h4 className='proj-title'>Innovative Ag Solutions</h4>
                     <ProjectPicture title='Innovative Ag Solutions' image={'../innovative.png'}/>
                     {/* <ProjectInfo title='Innovative Ag Solutions' titleDescription='Consultation web app for a PCA (Pest Control Advisor) to manage and consult their farmers/growers crops.' description='+ Centralized state management by using the Redux store to store all state information and for better scalability. + Engineered client side routing with BrowserRouter from the React-Router library to accelerate navigation. + Executed Single-Table-Inheritance to achieve the capability of having multiple user types (grower and salesman)' image={'../innovative.png'} technologies='Ruby on Rails, React and Redux, Semantic UI for styling.'/> */}
                     <button className='git-btn'>Github</button>
@@ -27,6 +29,7 @@ class Projects extends React.Component {
                     </div>
 
                     <div className='project-pics-and-info'>
+                    <h4 className='proj-title'>Optimal CRM</h4>
                     <ProjectPicture title='Optimal CRM' image={'../optimal.png'}/>
                     {/* <ProjectInfo title='Optimal CRM' titleDescription='Fake CRM with the ability for a salesrepresentative to keep track of leads, tasks, and companies.' description='+ Achieved the usage of model, view, controller (MVC) design pattern for separation of concerns within the application. + Demonstrated CRUD (create, read, update, delete) to implement a persistent storage application. + Maximized user security measures with implementing Bcrypt to safely store and salt user passwords.' technologies='Ruby on Rails'/> */}
                     <button className='git-btn'>Github</button>
