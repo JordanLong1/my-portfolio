58045fde43eeb4660b5a9808f601bee08cb31ea5
Jordan <jordanleelong5@gmail.com>
2020-10-22 15:34:46 -0700

    took off h4 header for pictures

--- src/Cards/ProjectPicture.js
+++ src/Cards/ProjectPicture.js
@@ -3,7 +3,6 @@ import React from 'react'
 export const ProjectPicture = props => {
     return (
         <div className='front-card'>
-            <h4>{props.title}</h4>
             <img className='project-images' src={props.image} alt=''/>
         </div>
     )
