d39ceaa39d21af9c7fbbfe24d15d370f27f5a99b
Jordan <jordanleelong5@gmail.com>
2020-10-23 10:08:59 -0700

    took out old links and implemented react scroll for smooth scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import {Home} from 'react-feather'
+import {Link} from 'react-scroll'
 class Navbar extends React.Component {
     render() {
         return (
@@ -8,9 +9,35 @@ class Navbar extends React.Component {
               <Home />
 
               <div className='nav-end'>
-              <Link to='/aboutme'>About Me</Link>
-              <Link to='/projects'>Projects</Link>
-              <Link to='/contactme'>Contact Me</Link>
+              <Link className='navbar-links'
+               activeClass='active'
+               to='aboutme'
+               spy={true}
+               smooth={true}
+               offset={0}
+               duration={1000}
+               >About Me
+               </Link>
+
+               <Link className='navbar-links'
+               activeClass='active'
+               to='projects'
+               spy={true}
+               smooth={true}
+               offset={0}
+               duration={1000}
+               >Projects
+               </Link>
+
+               <Link className='navbar-links'
+               activeClass='active'
+               to='contactme'
+               spy={true}
+               smooth={true}
+               offset={0}
+               duration={1000}
+               >Contact Me</Link>
+
               </div>
             </div>
         )
