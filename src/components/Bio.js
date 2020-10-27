5095c4639988115fc904fabc1aeb2658e5104979
Jordan <jordanleelong5@gmail.com>
2020-10-27 16:47:13 -0700

    removed old div

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -2,12 +2,10 @@ import React from 'react'
 
 export const Bio = () => {
     return (
-        <div className='bio-paragraph'>
             <p>
             Welcome to my portfolio site, My names Jordan Long I am from a small town called Orland, California right next to a better known place named Chico. After spending roughly 25 years in that area I receently moved to the Bay Area.
             With a background in the Agriculture Industry and physical fitness I am transitioning to tech with adaptability, intuition, and an endless amount of curiosity.
             I possess a strong passion for integrative problem solving, creative design, and reading and writing code that is aimed to fulfill a users experience. I am experienced with languages and frameworks such as Ruby on Rails, Javascript, React and Redux, HTML, and CSS. 
             </p>
-        </div>
     )
 }
