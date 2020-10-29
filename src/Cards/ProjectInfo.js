64c2cf0d8af3bd22cadc3fb04bc347c7d19aa592
Jordan <jordanleelong5@gmail.com>
2020-10-29 15:59:08 -0700

    added className to h4 for styling

--- src/Cards/ProjectInfo.js
+++ src/Cards/ProjectInfo.js
@@ -2,7 +2,8 @@ import React from'react'
 import '../Home.css'
 export const ProjectInfo = props => {
     return (
-        <div>
+        <div className='project-info-div'>
+            <h4 className='proj-info-h4'>{props.title}</h4>
      <p>{props.titleDescription}</p>
     <p>{props.description}</p>
     <li>{props.technologies}</li>
