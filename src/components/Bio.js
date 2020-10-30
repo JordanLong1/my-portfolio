5e186aade30b6b1ec614a69e8e193e194f6f5e4f
Jordan <jordanleelong5@gmail.com>
2020-10-29 17:57:02 -0700

    fixed unpropper grammar

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Bio = () => {
     return (
             <p>
-            Welcome to my portfolio site, My names Jordan Long I'm a coding bootcamp graudate (Flatiron School). I am from a small town called Orland, California right next to a better known place named Chico. After spending roughly 25 years in that area I receently moved to the Bay Area.
+            Welcome to my portfolio site, My name is Jordan Long and I'm a coding bootcamp graudate (Flatiron School). I am from a small town called Orland, California right next to a better known place named Chico. After spending roughly 25 years in that area I receently moved to the Bay Area.
             With a background in the Agriculture Industry and physical fitness I am transitioning to tech with adaptability, intuition, and an endless amount of curiosity.
             I possess a strong passion for integrative problem solving, creative design, and reading and writing code that is aimed to fulfill a users experience. 
             </p>
